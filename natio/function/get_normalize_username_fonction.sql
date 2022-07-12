7fafbe418db5ae0bcaf3e3ac5811ef56e8462c08
hugo <hugo.dealmeida@lephare.com>
2022-07-12 18:41:15 +0200

    fix(normalize_username): trim lastname

--- natio/function/get_normalize_username_fonction.sql
+++ natio/function/get_normalize_username_fonction.sql
@@ -1,9 +1,11 @@
-CREATE OR REPLACE FUNCTION get_normalize_username_fonction(idFonction Number, firstname CHAR, lastname CHAR)
+create or replace FUNCTION get_normalize_username_fonction(idFonction Number, firstname CHAR, lastname CHAR)
 RETURN CHAR
 IS
+    trim_lastname VARCHAR2(50);
     normalize_name VARCHAR2(50);
 BEGIN
-    SELECT CONCAT(CONCAT(UPPER(SUBSTR(F.NOMFONCTION, 1,1)),'_'), CONCAT(SUBSTR(firstname, 1,1), SUBSTR(lastname,1,3))) as name
+    trim_lastname := REGEXP_REPLACE(lastname, '\s');
+    SELECT CONCAT(CONCAT(UPPER(SUBSTR(F.NOMFONCTION, 1,1)),'_'), CONCAT(UPPER(SUBSTR(firstname, 1,1)), (UPPER(SUBSTR(trim_lastname,1,3))))) as name
     INTO normalize_name
     FROM FONCTION F
     WHERE F.NOFONCTION = idFonction;
@@ -11,4 +13,4 @@ BEGIN
     RETURN normalize_name;
 end;
 
-select rnatio.GET_NORMALIZE_USERNAME_FONCTION(3, 'Hugo', 'TARTINE') from DUAL;
+select rnatio.GET_NORMALIZE_USERNAME_FONCTION(3, 'Hugo', 'De Almeida') from DUAL;
